758996a0c60ff344fa3700ac714208c47ab8161a
gikey <xlp366@yahoo.com>
2016-09-09 17:34:36 +0800

    添加项目文件

---
+++ job/js/campus_24f4cac.js
@@ -0,0 +1,206 @@
+var position = {
+    0: {
+        name: "Android开发工程狮",
+        "duty-text": "招聘要求：",
+        "require-text": "工作职责：",
+        "position-require": "1. 在资深工程师指导下，从事简单Android app开发<br/>2. 在资深工程师指导下，完成部分功能代码单元测试和维护工作",
+        "position-duty": "1.计算机或相关专业本科及以上学历<br />2.熟悉Android开发<br />3.了解Linux系统优先<br />4.英语能力较好者优先<br />5. 对技术有浓厚兴趣，对开发工作有热情，愿意投入<br />6. 有半年以上，每周五天实习时间"
+    },
+    1: {
+        name: "iOS开发工程狮",
+        "duty-text": "招聘要求：",
+        "require-text": "工作职责：",
+        "position-require": "1. 在资深工程师指导下，从事简单iOS开发<br/>2. 在资深工程师指导下，完成部分功能代码单元测试和维护工作",
+        "position-duty": "1.计算机或相关专业本科及以上学历<br />2.熟悉iOS开发<br />3.了解Linux系统优先<br />4.英语能力较好者优先<br />5. 对技术有浓厚兴趣，对开发工作有热情，愿意投入<br />6. 有半年以上，每周五天实习时间"
+    },
+    2: {
+        name: "Python开发工程狮",
+        "duty-text": "招聘要求：",
+        "require-text": "工作职责：",
+        "position-require": "1.在资深工程师指导下，从事简单Python开发<br/>2.在资深工程师指导下，完成部分功能代码单元测试和维护工作",
+        "position-duty": "1.计算机或相关专业本科及以上学历<br />2.熟悉Python开发<br />3.了解Linux系统优先<br />4.英语能力较好者优先<br />5. 对技术有浓厚兴趣，对开发工作有热情，愿意投入<br />6. 有半年以上，每周五天实习时间"
+    },
+    3: {
+        name: "C++开发工程狮",
+        "duty-text": "招聘要求：",
+        "require-text": "工作职责：",
+        "position-require": "1.在资深工程师指导下，从事简单C++开发<br/>2.在资深工程师指导下，完成部分功能代码单元测试和维护工作",
+        "position-duty": "1.计算机或相关专业本科及以上学历<br />2.具有C++基础，对音视频方向有兴趣者优先<br />3.了解Linux系统优先<br />4.英语能力较好者优先<br />5. 对技术有浓厚兴趣，对开发工作有热情，愿意投入<br />6. 有半年以上，每周五天实习时间"
+    },
+    4: {
+        name: "测试工程狮",
+        "duty-text": "招聘要求：",
+        "require-text": "工作职责：",
+        "position-require": "1.在资深工程师指导下，从事简单测试开发工作<br/>2.在资深工程师指导下，完成部分功能代码单元测试和维护工作",
+        "position-duty": "1.计算机或相关专业本科及以上学历<br />2.熟悉测试工作流程<br />3.了解Linux系统优先<br />4.英语能力较好者优先<br />5. 对技术有浓厚兴趣，对开发工作有热情，愿意投入<br />6. 有半年以上，每周五天实习时间"
+    },
+    5: {
+        name: "运维工程狮",
+        "duty-text": "招聘要求：",
+        "require-text": "工作职责：",
+        "position-require": "1.在资深工程师指导下，从事简单运维工作",
+        "position-duty": "1.计算机或相关专业本科及以上学历<br />2.对运维工作感兴趣<br />3.了解Linux系统优先<br />4.英语能力较好者优先<br />5. 对技术有浓厚兴趣，对开发工作有热情，愿意投入<br />6. 有半年以上，每周五天实习时间"
+    },
+    6: {
+        name: "web前端工程狮",
+        "duty-text": "招聘要求：",
+        "require-text": "工作职责：",
+        "position-require": "1.在资深工程师指导下，从事简单web开发工作<br/>2.在资深工程师指导下，完成部分功能代码单元测试和维护工作",
+        "position-duty": "1.计算机或相关专业本科及以上学历<br />2.熟悉web开发<br />3. 英语能力较好者优先<br />4.对技术有浓厚兴趣，对开发工作有热情，愿意投入<br />5. 有半年以上，每周五天实习时间"
+    },
+    7: {
+        name: "产品汪",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.协助产品经理完成产品原型<br/>2.分析产品数据，对产品的发展提出合理建议",
+        "position-require": "1.17届应届毕业生，本科以上学历，计算机相关专业优先<br />2.英语六级以上，良好的读写能力<br />3.良好的沟通能力，有强烈的好奇心，逻辑思维能力强<br />4.熟悉移动互联网，熟悉常用的产品设计工具"
+    },
+    8: {
+        name: "运营喵",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.协助产品经理完成产品原型<br/>2.分析产品数据，运营分析，对产品的发展提出合理建议",
+        "position-require": "1.17届应届毕业生，本科以上学历，计算机相关专业优先<br />2.英语六级以上，良好的读写能力<br />3.良好的沟通能力，有强烈的好奇心，逻辑思维能力强<br />4.熟悉移动互联网，熟悉常用的产品设计工具"
+    },
+    9: {
+        name: "UI设计师",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.在UI设计师的指导下完成产品视觉设计工作<br/>2.参与产品策划、设计与原型制作<br/>3.与兄弟团队密切配合，完成产品的设计与开发<br/>4.对移动互联网产品有独到理解，能够对产品提供专业的意见和建议",
+        "position-require": "1.17届应届毕业生，本科及以上学历，视觉设计、美术相关专业<br />2.脑洞大，想法多，熟悉移动互联网营销特点及趋势<br />3.具有良好的沟通, 协作能力和丰富的团队工作经验, 逻辑思维清晰, 做事认真, 细致，能承受较大的工作压力"
+    },
+    10: {
+        name: "广告设计师",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.理解不同海外地区市场的美术素材风格，设计和优化各种宣传素材<br/>2.负责电商的各类宣传、推广资料等视觉创意设计<br/>3.分析用户行为和用户心理，设计出更适合互联网平台投放的广告<br/>4.持续优化广告创意",
+        "position-require": "1.17届应届毕业生，本科及以上学历，视觉设计、美术相关专业<br />2.脑洞大，想法多，熟悉移动互联网营销特点及趋势<br />3.会PS、AE， 能够制作相应动静态广告，会视频剪辑者优先考虑<br />4. 良好的设计能力, 色彩感、形式感强，思路开阔，创意丰富"
+    },
+    11: {
+        name: "市场专员",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.日常微信群维护，与粉丝拉近距离<br/>2.官方微博辅助更新与维护，上班时间光明正大织围脖<br/>3.跟随市场团队一同不定期挖脑洞，共享最新鲜的想法<br/>4.配合市场活动出展，遍访各大展会活动",
+        "position-require": "1.17届应届毕业生，本科及以上学历<br />2.有学生会干部、社团团长相关经验者优先<br />3.大学期间多次组织、参加各类企业或校方活动优先<br />4. 工作积极主动，有责任心，学习能力强<br />5. 良好的团队协作沟通能力，人际沟通能力<br />6. 具有软件、互联网开发者社区资源优先"
+    },
+    12: {
+        name: "商务销售",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.拓展国内出海客户<br/>2.维护客户关系<br/>3.参加展会，找目标客户",
+        "position-require": "1.17届应届毕业生，本科以上学历<br />2.热爱移动互联网行业，英语六级以上水平<br />3.具有较强的沟通协调能力和语言表达能力<br />4. 较强的亲和力，团队合作精神<br />5.较强的学习能力，抗压性强"
+    },
+    13: {
+        name: "国内商务",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.负责拓展国内面向海外市场的移动开发者<br/>2.跟踪及处理客户反馈，维护客户关系<br/>3.负责国内市场跟国际市场信息的收集和竞争对手的分析<br/>4.完成客户的合同谈判，合同签订",
+        "position-require": "1.17届应届毕业生，本科以上学历<br />2.工作积极主动、严谨细致，为人诚信，责任心强，富有开拓精神<br />3. 热爱互联网及移动互联网，对于移动互联网商务模式有初步的认识<br />4. 熟练使用office办公软件"
+    },
+    14: {
+        name: "广告优化师",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.负责Facebook、Adwords平台的日常广告投放及优化<br/>2.深入了解各类型应用或游戏的用法、特色，跟进广告素材的设计、制作<br/>3.实时监控广告数据，并作分析、统计,及时调整投放策略<br/>4.定期输出投放数据和用户分析数据报表",
+        "position-require": "1.17届应届毕业生，本科以上学历<br />2.英语6级及以上，具备很强的读写能力<br />3.具有良好的条理性和逻辑性，有一定的数据分析能力<br />4. 熟练操作OFFICE办公软件，精通EXCEL、PPT等办公工具<br/>5.执行能力强，有较强的学习、创新意识"
+    },
+    15: {
+        name: "财务专员",
+        "duty-text": "岗位职责：",
+        "require-text": "任职资格：",
+        "position-duty": "1.协助处理、审核相关凭证，并完成装订与整理<br/>2.协助完成财务部门内的财务、会计、统计等工作<br/>3.协助管理公司合同、发票及账单等<br/>4.办理银行、税务等相关外勤事宜<br/>5.处理员工各类费用报销",
+        "position-require": "1.17届应届毕业生，本科及以上学历，财务、会计相关专业<br />2.熟练使用办公软件，Excel能够达到精通尤佳<br />3.为人正直，责任心强，对工作认真负责<br />4.良好的团队合作能力"
+    },
+    16: {
+        name: "投资专员",
+        "duty-text": "岗位职责：",
+        "require-text": "任职资格：",
+        "position-duty": "1.研究相关行业数据和信息<br/>2.协助进行项目筛选、分析和尽调工作<br/>3.协助同事完成其他工作",
+        "position-require": "1.17届应届毕业生，本科及以上学历，金融相关专业<br />2.英语六级以上，有较好的英语水平<br />3.有足够的好奇心，对投资感兴趣<br />4. 认真靠谱，有优秀的团队协作精神和责任心<br />5. 熟练使用word、excel、ppt等办公软件"
+    },
+    17: {
+        name: "法务专员",
+        "duty-text": "岗位职责：",
+        "require-text": "任职资格：",
+        "position-duty": "1.负责公司合同与法律文件的审核<br/>2.负责公司日常法律工作<br/>3.负责与外聘律师、外部法律顾问的沟通与合作<br/>4.处理公司各类诉讼或非诉讼法律事务，维护公司合法权益<br/>5.参与公司其他日常事务",
+        "position-require": "1.17届应届毕业生，本科及以上学历，法律相关专业<br />2.英语六级以上，英语听说读写流利<br />3.思维严谨，对工作认真负责，具有独立思考问题、客观分析问题的能力<br />4. 能够熟练使用WORD、EXCEL及PPT等办公软件"
+    },
+    18: {
+        name: "商业化专员",
+        "duty-text": "职位描述：",
+        "require-text": "任职要求：",
+        "position-duty": "1.跟踪广告业务数据，协助BD完成广告的上线以及日后的每天维护<br/>2.懂得挖掘每个广告主的潜在价值以及协助BD完成KPI以及业务增长<br/>3.数据分析，及时跟进业务进度和业务结果反馈",
+        "position-require": "1.17届应届毕业生，本科以上学历<br />2.英语六级以上，良好的读写能力<br />3.具有良好的沟通, 协作能力和丰富的团队工作经验<br />4.逻辑思维清晰, 做事认真, 细致, 能承受较大的工作压力"
+    }
+};
+$(function () {
+    function e() {
+        $(".banner-wrapper").hasClass("details-banner") || $(".banner-wrapper").css("height", l + "px")
+    }
+
+    function t() {
+        var e = $(".campus-menu ul li.current");
+        $(e).append(u), r(e)
+    }
+
+    function r(e) {
+        var t = $(e).attr("data-layerid");
+        $("#" + t).show()
+    }
+
+    function o() {
+        var e = window.location.hash;
+        null == e || ("#campus" == e ? $(".campus-menu ul li.campus").click() : "#jobs" == e ? $(".campus-menu ul li.jobs").click() : "#rule" == e ? $(".campus-menu ul li.rule").click() : "#story" == e && $(".campus-menu ul li.story").click())
+    }
+
+    function i() {
+        $(".qr_modal").css("opacity", "1"), $("#download_qr_overlay").addClass("qr_show_layer"), $("#download_qr").addClass("qr_show")
+    }
+
+    function n() {
+        $("#download_qr_overlay").removeClass("qr_show_layer"), $("#download_qr").removeClass("qr_show")
+    }
+
+    function a(e) {
+        var t = new RegExp("(^|&)" + e + "=([^&]*)(&|$)", "i"), r = window.location.search.substr(1).match(t);
+        return null != r ? unescape(r[2]) : null
+    }
+
+    function s() {
+        var e = a("jid");
+        if (null != e) {
+            var t = position[e], r = t.name, o = t["duty-text"], i = t["position-duty"], n = t["require-text"], s = t["position-require"];
+            $(".jobs-name").html(r), $(".duty-text").html(o), $(".position-duty").html(i), $(".require-text").html(n), $(".position-require").html(s)
+        }
+    }
+
+    var l = $(".banner-wrapper").height(), u = '<img src="http://static.activities.apuslauncher.com/upload/fe/20160818104815c8f053b31f.png">', d = 200;
+    e(), t(), $(".campus-menu ul li").on("click", function () {
+        if ($(this).hasClass("current")); else {
+            $(this).addClass("current").append(u), $(this).siblings("li").removeClass("current").children("img").remove();
+            var e = $(this).attr("data-layerid");
+            $("#" + e).show().siblings("div").hide();
+            var t = $(".campus-menu ul").height();
+            $("body,html").animate({scrollTop: l - t + "px"}, d)
+        }
+    }), o(), $(".copyright a.btn-weixin").on("click", function () {
+        i()
+    }), $("a.closed-qr").on("click", function () {
+        n()
+    }), $("div.qr_overlay").on("click", function () {
+        n()
+    }), $(".campus-jobs div ul li").on("click", function () {
+        var e = $(this).attr("data-jobsid");
+        window.location.href = "jobs-details.html?jid=" + e
+    }), s();
+    var b = function (e) {
+        var t = document.createElement("b");
+        return t.innerHTML = "<!--[if IE " + e + "]><i></i><![endif]-->", 1 === t.getElementsByTagName("i").length
+    };
+    window.onscroll = function () {
+        var e = document.documentElement.scrollTop || document.body.scrollTop, t = $(".campus-menu"), r = t.height();
+        e >= l - r ? $(t).hasClass("fixed") || $(t).addClass("fixed") : $(t).removeClass("fixed"), $(window).scrollTop() > 200 ? b(6) || b(7) || b(8) || b(9) ? $("#back-to-top").css({display: "block"}) : $("#back-to-top").fadeIn(500) : b(6) || b(7) || b(8) || b(9) ? $("#back-to-top").css({display: "none"}) : $("#back-to-top").fadeOut(500)
+    }, $("#back-to-top").on("click", function () {
+        return $("body,html").animate({scrollTop: 0}, 700), !1
+    })
+});
